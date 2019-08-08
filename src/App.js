ffa3ecc363f72fb39aa6cda675eb687de7981fc0
Jonathan Meirelles <jonathanms30@gmail.com>
2019-08-08 17:34:58 -0300

    Dropdown Header
    Feito o dropdown do botão de configurações do header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './ui/ui.css';
 import './App.css';
 import Login from './component/login/login';
 import MenuLateral from './component/menuLateral/menuLateral';
+import Header from './component/header/header';
 // import axios from 'axios';
 
 // const api = axios.create({
@@ -16,7 +17,7 @@ function App(props) {
   return (
     <div className="App">
       {/* <Login></Login> */}
-      <MenuLateral></MenuLateral>
+      <Header></Header>
     </div>
   );
 }
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
@@ -7,7 +8,25 @@ export default function Header() {
             <div className='container'>
                 <div className='headerContent'>
                     <img className='logoImg' src={require('../../assets/img/logo.svg')} alt='logo'></img>
-                    <img className='configMenuBtn' src={require('../../assets/img/icon-settings.svg')} alt='settings'></img>
+                    {/* <div className='dropdown'>
+                        <a href='#'><img className='configMenuBtn' src={require('../../assets/img/icon-settings.svg')} alt='settings'></img></a>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                                <a href='#'>Link 4</a>
+                            </div>
+                    </div> */}
+                    <div class="dropdown">
+                        {/* <img className='dropbtn' src={require('../../assets/img/icon-settings.svg')} alt='settings'></img> */}
+                        <button class="dropbtn"></button>
+                        <div class="dropdown-content">
+                            <Link to='/' href="#">Configurações</Link>
+                            <Link to='/' href="#">Log do Sistema</Link>
+                            <Link to='/' href="#">Log da Conta</Link>
+                            <Link to='/' href="#">Sair</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>
