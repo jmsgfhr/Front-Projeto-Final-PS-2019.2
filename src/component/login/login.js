9601b2445c543f2185fe06d54383848f5b188f93
Jonathan Meirelles <jonathanms30@gmail.com>
2019-08-07 21:59:36 -0300

    Alteraçoes da tela de login
    Adicionado os items basicos da tela de login

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './login.css'
+import PanelLeft from '../panelLeft/panelLeft';
 
 export default function Login(props) {
     return  (
         <section className='login'>
+            <PanelLeft></PanelLeft>
             <div className='loginContent'>
                 <h2 className='formTitle'>Área do treinamento</h2>
                 <form className='formLogin'>
                     <input className='loginName' placeholder='login'></input>
+                    <input className='loginPassword' placeholder='senha'></input>
+                    <button className='loginSubmit' >ENTRAR</button>
                 </form>
             </div>
         </section>
