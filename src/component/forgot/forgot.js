c5f338c4be9b1e0b289afccf973190429e5eecd2
Jonathan Meirelles <jonathanms30@gmail.com>
2019-08-08 11:25:53 -0300

    Paginas de login e esqueci a senha
    Feito as paginas de login e esqueci a senha

--- src/component/forgot/forgot.js
+++ src/component/forgot/forgot.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './forgot.css';
 import PanelLeft from '../panelLeft/panelLeft';
 
 export default function Forgot(props) {
     return  (
-        <section className='login'>
+        <section className='forgot'>
             <PanelLeft></PanelLeft>
             <div className='forgotContent'>
                 <h2 className='formTitle'>Recuperar senha</h2>
-                <form className='formLogin'>
+                <form className='formForgot'>
                     <input className='forgotEmail' placeholder='email'></input>
                     <button className='forgotSubmit' >ENVIAR</button>
                 </form>
