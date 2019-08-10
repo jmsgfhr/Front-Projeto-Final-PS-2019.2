09feaee7d7948ab39c8c74451b31e84a0a659d11
Jonathan Meirelles <jonathanms30@gmail.com>
2019-08-10 01:25:34 -0300

    Refeito a adição do usuario na tabela
    Removido o AJAX e incluido o metodo em JSX

--- src/component/indexInstrutor/indexInstrutor.js
+++ src/component/indexInstrutor/indexInstrutor.js
@@ -1,97 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import './indexInstrutor.css';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../../App';
 import UserMenu from '../../ui/ui';
-
-
-const api = axios.create({
-  baseURL: 'https://projeto-final-in.herokuapp.com/users',
-});
+import api from '../../api';
 
 
 export default class IndexInstrutor extends React.Component {
 
-  render (){
-      return(
-          <div className='indexInstrutorForm'>
-          <div className='searchBar'>
-              <h2 className='searchTitle'>Usuário</h2>
-              <input className='inputSearch' placeholder='Pesquisa' onKeyUp={() => this.searchFunction()}></input>
-          </div>
-          </div>
-      )
-  }
+  state = {users: []};
+
   componentDidMount() {
-      api.get('/')
-      .then(function (response) {
-        let table = document.querySelector('.indexInstrutorForm');
-        let tableUsers = document.createElement('table');
-        tableUsers.classList.add('tableUser');
-        let tableRow = document.createElement('tr');
-        let tableUserName = document.createElement('th');
-        tableUserName.innerText = 'Nome do Usuário';
-        let tableUserType = document.createElement('th');
-        tableUserType.innerText = 'Tipo de Usuario';
-        let tableUserStatus = document.createElement('th');
-        tableUserStatus.innerText = 'Status';
-        let tableUserEdit = document.createElement('th');
-        tableRow.appendChild(tableUserName);
-        tableRow.appendChild(tableUserType);
-        tableRow.appendChild(tableUserStatus);
-        tableRow.appendChild(tableUserEdit);
-        tableUsers.appendChild(tableRow);
-        table.appendChild(tableUsers);
-        for (let i = 0; i < response.data.length; i++) {
-            let tableRow = document.createElement('tr');
-            tableRow.classList.add('tableRowUser');
-            let tableUserName = document.createElement('td');
-            tableUserName.innerText = response.data[i].name;
-            tableUserName.classList.add('tdName');
-            let tableUserType = document.createElement('td');
-            tableUserType.innerText = response.data[i].kind;
-            let tableUserStatus = document.createElement('td');
-            let userStatus = document.createElement('span');
-            userStatus.classList.add('spanStatus');
-            userStatus.classList.add('red');
-            userStatus.innerText = response.data[i].kind;
-            tableUserStatus.appendChild(userStatus);
-            let tableUserEdit = document.createElement('td');
-            ReactDOM.render(<BrowserRouter><Link to="/404" target={UserMenu} className='editBtn'>editar</Link></BrowserRouter>, tableUserEdit);
-            tableRow.appendChild(tableUserName);
-            tableRow.appendChild(tableUserType);
-            tableRow.appendChild(tableUserStatus);
-            tableRow.appendChild(tableUserEdit);
-            tableUsers.append(tableRow);      
-        }   
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .finally(function () {
+      api.get('/users').then(response => {
+        this.setState({ users: [...response.data]});
       });
   }
+
+  render (){
+    const user = this.state.users.map((user) => {
+      return(
+        <tr className='tableRowUser'>
+          <td className='tdName'>{user.name}</td>
+          <td>{user.kind}</td>
+          <td><span className='spanStatus red'>{user.kind}</span></td>
+          <td><Link to='/404' className='editBtn'>editar</Link></td>
+        </tr>
+      )
+    });
+    
+    return(
+        <div className='indexInstrutorForm'>
+        <div className='searchBar'>
+            <h2 className='searchTitle'>Usuário</h2>
+            <input className='inputSearch' placeholder='Pesquisa' onKeyUp={() => this.searchFunction()}></input>
+        </div>
+
+        <table className='tableUser'>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Tipo</th>
+                  <th>Status</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                  {user}
+              </tbody>
+            </table>
+        </div>
+    )
+}
+
   searchFunction = () => {
-    
     var input, filter, list, tableSearch, a, i, txtValue;
-
     input = document.querySelector('.searchBar .inputSearch');
-
     filter = input.value.toUpperCase();
-
     list = document.querySelector(".indexInstrutorForm");
-
     tableSearch = document.querySelectorAll('.tableRowUser');
-    console.log(tableSearch);
-    
-
     for (i = 0; i < tableSearch.length; i++) {
         a = tableSearch[i].querySelector(".tdName");
         txtValue = a.innerText;
-        
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             tableSearch[i].style.display = "";
         } else {
