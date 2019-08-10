92ac2ba63003e476937b07dfb184a65fc78ee1a5
Jonathan Meirelles <jonathanms30@gmail.com>
2019-08-10 20:26:40 -0300

    Ajustes
    Criado uma pagina de criação de usuario

--- src/component/panelLeft/panelLeft.js
+++ src/component/panelLeft/panelLeft.js
@@ -9,7 +9,7 @@ export default function PanelLeft(props) {
             <div className='panelLeftContent'>
                 <h2 className='panelLeftTitle'>Processo Seletivo</h2>
                 <p className='panelLeftDescription'>Está aberto o processo seletivo de 2019.2 As inscrições irão de 01/01 até 01/01.</p>
-                <Link to="/404" className='inscription'>Increva-se</Link>
+                <Link to="/subscribe" className='inscription'>Increva-se</Link>
             </div>
         </div>
     )
