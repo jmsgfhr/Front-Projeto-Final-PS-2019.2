a5465de7199954471c5d3e9272d8e65b2355e4d0
jonathan <jonathanms30@gmail.com>
2021-02-22 16:52:54 -0300

    add files

---
+++ src/component/forgot/forgot.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './forgot.css';
+import PanelLeft from '../panelLeft/panelLeft';
+
+export default function Forgot(props) {
+    return  (
+        <section className='forgot'>
+            <PanelLeft></PanelLeft>
+            <div className='forgotContent'>
+                <h2 className='formTitle'>Recuperar senha</h2>
+                <form className='formForgot'>
+                    <input className='forgotEmail' placeholder='email'></input>
+                    <button className='forgotSubmit' >ENVIAR</button>
+                </form>
+            </div>
+        </section>
+    )
+}
---
+++ src/component/panelLeft/panelLeft.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './panelLeft.css';
+import { Link } from 'react-router-dom';
+
+export default function PanelLeft(props) {
+    return (
+        <div className='panelLeft'>
+            <img className='logoPanel' src={require('../../assets/img/logo.svg')} alt='logo'></img>
+            <div className='panelLeftContent'>
+                <h2 className='panelLeftTitle'>Processo Seletivo</h2>
+                <p className='panelLeftDescription'>Está aberto o processo seletivo de 2019.2 As inscrições irão de 01/01 até 01/01.</p>
+                <Link to="/404" className='inscription'>Increva-se</Link>
+            </div>
+        </div>
+    )
+}
